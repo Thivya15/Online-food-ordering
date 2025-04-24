0191263ad384190f6fff6d879701a09558b71bc4
Thivya15 <thivyamahendran15@gmail.com>
2025-04-24 08:13:10 +0530

    create tho online food ordering app eith models and security features

---
+++ src/main/java/com/thivya/OnlineFoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.thivya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingApplication.class, args);
+	}
+
+}
